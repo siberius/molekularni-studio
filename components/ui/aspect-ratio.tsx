f88c609ec78ac37f0f14be9682708961658c30d5
Luděk <ludek.susicky@gmail.com>
2026-01-21 19:49:54 +0100

    Add files via upload

--- components/ui/aspect-ratio.tsx
+++ components/ui/aspect-ratio.tsx
@@ -1,7 +1,2 @@
 'use client';
-
-import * as AspectRatioPrimitive from '@radix-ui/react-aspect-ratio';
-
-const AspectRatio = AspectRatioPrimitive.Root;
-
-export { AspectRatio };
+export default function Placeholder() { return null; }
